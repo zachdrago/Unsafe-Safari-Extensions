9c66b35dbdc491d19b5bc5abab0c1f72c2b86486
Zach Drago <zachdrago@gmail.com>
2019-03-27 17:52:38 -0400

    Create tay2.js

---
+++ tay2.js
@@ -0,0 +1,35 @@
+setTimeout(function(){
+
+	if( $('body').hasClass('wsite-blog-index') ) {
+
+		$('.blog-date').each(function() {
+
+			var season;
+
+
+			var blog_date = $(this).find('.date-text');
+			var date_split = blog_date.split('/');
+
+			var month = date_split[0];
+			var year = date_split[2];
+
+			console.log(month+', '+year);
+
+
+			if(month >= 8 && month <= 12) {
+				season = "Fall";
+			}
+			if(month >= 1 && month <= 5) {
+				season = "Spring";
+			}
+
+
+			$(this).find('.date-text').text(season+' | '+year);
+
+
+		});
+		
+
+	}
+
+}, 2500);
