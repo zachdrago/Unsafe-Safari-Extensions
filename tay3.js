b01b4135a59a3b1d9166335e0c9c373d0e03573a
Zach Drago <zachdrago@gmail.com>
2019-03-27 17:59:48 -0400

    Create tay3.js

---
+++ tay3.js
@@ -0,0 +1,41 @@
+setTimeout(function(){
+
+	if( $('body').hasClass('wsite-blog-index') ) {
+
+		$('.blog-date').each(function() {
+
+			var season;
+
+
+			var blog_date = $(this).find('.date-text').text();
+			var date_split = blog_date.split('/');
+
+			var month = date_split[0];
+			var year = date_split[2];
+
+			console.log(month+', '+year);
+
+
+			if(month >= 8 && month <= 12) {
+				season = "Fall";
+			}
+			if(month >= 1 && month <= 5) {
+				season = "Spring";
+			}
+
+			var newDateformat = season+' | '+year;
+
+			$(this).find('.date-text').text(newDateformat);
+			$('<p class="semester">'+newDateformat+'</p>').insertAfter( $(this) );
+
+		});
+		
+
+	}
+
+}, 2500);
+
+
+
+
+
