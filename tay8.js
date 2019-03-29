4c2b1ff7d2c84513b77d6d0335b8e4acd1279d2e
Zach Drago <zachdrago@gmail.com>
2019-03-29 13:56:48 -0400

    Create tay8.js

---
+++ tay8.js
@@ -0,0 +1,39 @@
+setTimeout(function(){
+
+
+
+
+// ================== BLOG POST PAGES ===========================
+
+	if( $('body').hasClass('wsite-blog-post') ) {
+
+			var season;
+
+			var blog_date = $('.date-text').text();
+			var date_split = blog_date.split('/');
+
+			var month = date_split[0];
+			var year = date_split[2];
+
+
+
+			if(month >= 8 && month <= 12) {
+				season = "Fall";
+			}
+			if(month >= 1 && month <= 5) {
+				season = "Spring";
+			}
+
+
+			var newDateformat = season+' '+year;
+
+			$('.date-text').text(newDateformat);
+			$('<p class="semester">'+newDateformat+'</p>').insertAfter( $('.blog-date') );
+		
+	}
+
+
+
+
+
+}, 2500);
